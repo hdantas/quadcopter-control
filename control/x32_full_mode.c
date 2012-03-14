bcf211d605e32c50580fed40019c741b9603b6a8
Henrique Dantas <hndantas@gmail.com>
2012-03-14 22:16:30 +0100

    Debugging-purpose changes

--- control/x32_full_mode.c
+++ control/x32_full_mode.c
@@ -5,12 +5,24 @@
 #include "comm.h"
 #include "x32_modes.h"
 #include "kalman.h"
+	
+#define CAPACITY 256
+#define CAPACITY_SHIFT 8 //256 = 2^8	
 
+int latency[CAPACITY];
+int i = 0;
+int j = 0;
+int starttime = 0;
+int endtime = 0;
+int average = 0;
+	
 void full_mode_ctrl(void)
 {
 	//roll = p1_full * (roll - s0) - p2_full * s3;
 	//pitch = p1_full * (pitch - s1) - p2_full * s4;	
 
+	starttime = X32_us_clock;
+	
 	yaw_mode_ctrl();
 	
 	kalman_filter_roll();
@@ -18,5 +30,19 @@ void full_mode_ctrl(void)
 
 	kalman_filter_pitch();
 	pitch = p1_full * (pitch - theta_kalman_pitch) - p2_full * q_kalman_pitch;	
+	
+	endtime=X32_us_clock;
+	
+	latency[i++] = endtime-starttime;
+
+	if (i == CAPACITY - 1)
+	{	
+		average = 0;
+		for (j=0; j<CAPACITY; j++)
+			average += latency[j];
+		average = average << CAPACITY_SHIFT;
+		printf("Average control latency : %d\n",average);
+		i=0;
+	}	
 
 }
